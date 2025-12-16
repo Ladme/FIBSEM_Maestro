0148eb9eee9e528236232e1fd62fdf0a5a456ba7
Ladme <ladmeb@gmail.com>
2025-12-16 14:08:30 +0100

    Doc string for TileCoordinates

--- src/fibsem_maestro/core/tile_coordinates.py
+++ src/fibsem_maestro/core/tile_coordinates.py
@@ -9,7 +9,7 @@ from fibsem_maestro.core.point import PixelPoint
 
 @dataclass(frozen=True)
 class TileCoordinates:
-    """Top-left coordinate and size of a tile."""
+    """Top-left coordinate and pixel size of a tile."""
 
     origin: PixelPoint
     width_px: int
