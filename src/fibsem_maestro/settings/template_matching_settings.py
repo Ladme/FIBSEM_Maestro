2b5037f70f629ec75c95da63ac7d40895fb4ba2b
Ladme <ladmeb@gmail.com>
2026-04-17 14:07:26 +0200

    Restructuring TemplateMatchingSettings

--- src/fibsem_maestro/settings/template_matching_settings.py
+++ src/fibsem_maestro/settings/template_matching_settings.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2024-2025 CEMCOF
 
 from pathlib import Path
-from typing import Annotated
 
 from pydantic import Field
 
@@ -13,70 +12,43 @@ from fibsem_maestro.settings.property_names import PropertyNames
 
 
 class TemplateMatchingSettings(BaseSettings):
-    properties_file: Annotated[
-        Path,
-        Field(
-            default=Path("template_matching_props.yaml"),
-            description="Path to a file storing properties of the microscope used for drift correction imaging.",
-        ),
-    ]
-    templates_directory: Annotated[
-        Path,
-        Field(
-            default=Path("template_matching"),
-            description="Path to a directory where templates will be saved.",
-        ),
-    ]
-    properties_to_collect: Annotated[
-        PropertyNames,
-        Field(
-            default_factory=PropertyNames,
-            description="Properties of the microscope and the beam relevant for drift correction.",
-        ),
-    ]
-    beam_type: Annotated[
-        BeamType,
-        Field(
-            default=BeamType.ELECTRON,
-            description="Beam used for drift correction imaging.",
-        ),
-    ]
-    areas: Annotated[
-        list[RelativeArea],
-        Field(
-            default_factory=list,
-            description="Areas of the image used for template matching defined in relative units.",
-        ),
-    ]
-    min_confidence: Annotated[
-        float,
-        Field(
-            default=0.8,
-            description="Minimal cross-correlation value required for a template match to be accepted as a valid drift measurement.",
-        ),
-    ]
-    rescan: Annotated[
-        int,
-        Field(default=50, description="Number of slices between template refreshing."),
-    ]
-    blur: Annotated[
-        int,
-        Field(
-            default=3,
-            description="Standard deviation (in pixels) of a Gaussian filter applied to the image before template matching.",
-        ),
-    ]
-    correction_margin: Annotated[
-        float,
-        Field(
-            default=20000,
-            description="The maximum expected drift in nanometers defining how far the template is allowed to search for a match.",
-        ),
-    ]
-    stop_acquisition_at_failure: Annotated[
-        bool,
-        Field(
-            default=False,
-            description="Should image acquisition be stopped if drift correction fails?",
-        ),
-    ]
+    properties_file: Path = Field(
+        default=Path("template_matching_props.yaml"),
+        description="Path to a file storing properties of the microscope used for drift correction imaging.",
+    )
+    templates_directory: Path = Field(
+        default=Path("template_matching"),
+        description="Path to a directory where templates will be saved.",
+    )
+    properties_to_collect: PropertyNames = Field(
+        default_factory=PropertyNames,
+        description="Properties of the microscope and the beam relevant for drift correction.",
+    )
+    beam_type: BeamType = Field(
+        default=BeamType.ELECTRON,
+        description="Beam used for drift correction imaging.",
+    )
+    areas: list[RelativeArea] = Field(
+        default_factory=list,
+        description="Areas of the image used for template matching defined in relative units.",
+    )
+    min_confidence: float = Field(
+        default=0.8,
+        description="Minimal cross-correlation value required for a template match to be accepted as a valid drift measurement.",
+    )
+    rescan: int = Field(
+        default=50,
+        description="Number of slices between template refreshing.",
+    )
+    blur: int = Field(
+        default=3,
+        description="Standard deviation (in pixels) of a Gaussian filter applied to the image before template matching.",
+    )
+    correction_margin: float = Field(
+        default=20000,
+        description="The maximum expected drift in nanometers defining how far the template is allowed to search for a match.",
+    )
+    stop_acquisition_at_failure: bool = Field(
+        default=False,
+        description="Should image acquisition be stopped if drift correction fails?",
+    )
