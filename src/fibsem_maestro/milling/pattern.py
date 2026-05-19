4b9e35769450c1f72c8aed9ded142287e0ed9f00
Ladme <ladmeb@gmail.com>
2026-05-19 16:46:10 +0200

    Removed unused pattern type

--- src/fibsem_maestro/milling/pattern.py
+++
@@ -1,9 +0,0 @@
-# Released under MIT License.
-# Copyright (c) 2024-2025 CEMCOF
-
-from enum import Enum
-
-
-class PatternType(str, Enum):
-    CLEANING_CROSS_SECTION_PATTERN = "ccs"
-    REGULAR_CROSS_SECTION_PATTERN = "rcs"
