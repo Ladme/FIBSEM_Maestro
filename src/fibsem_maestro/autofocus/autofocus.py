750e13abb2bfc3cc4cc026afe9197ef8006da8dd
Ladislav Bartos <ladmeb@gmail.com>
2026-09-07 09:26:56 +0200

    fix: grab full frame after autofocus test so the user sees what the image will look like

--- src/fibsem_maestro/autofocus/autofocus.py
+++ src/fibsem_maestro/autofocus/autofocus.py
@@ -5,7 +5,6 @@
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import field
-from time import sleep
 from typing import TYPE_CHECKING, Any
 
 import numpy as np
@@ -276,9 +275,7 @@ class Autofocus(Action[AutofocusSettings, AutofocusState]):
         self._run_sweep_and_apply_best()
 
         # the updated value may not be displayed in the microscope GUI until we start scanning
-        self._microscope.beam.start_acquisition()
-        sleep(1)
-        self._microscope.beam.stop_acquisition()
+        self._microscope.beam.grab_frame()
 
         self._ctx.text_logger.info(f"Completed test for {self.name}.")
 
