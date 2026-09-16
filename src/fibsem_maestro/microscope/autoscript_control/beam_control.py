e1ff00c316f00fa38d277f710c6445bad94499ab
Ladme <ladmeb@gmail.com>
2026-09-16 08:49:42 +0200

    fix: getting from extended resolution mode

--- src/fibsem_maestro/microscope/autoscript_control/beam_control.py
+++ src/fibsem_maestro/microscope/autoscript_control/beam_control.py
@@ -59,6 +59,9 @@ class AutoscriptBeamControl(BeamControl, Generic[BeamT]):
         )
         self._txt_log = txt_log
 
+        self._extended_resolution: Resolution | None = (
+            None  # extended resolution is set only if the required resolution is not standard
+        )
         self._standard_resolutions = (
             Resolution(1024, 884),
             Resolution(1536, 1024),
@@ -369,24 +372,40 @@ class AutoscriptBeamControl(BeamControl, Generic[BeamT]):
 
     @property
     def resolution(self) -> Resolution:
-        res = Resolution(
-            width=self._beam.scanning.resolution.width,
-            height=self._beam.scanning.resolution.height,
-        )
+        if self._extended_resolution is None:
+            x, y = (
+                self._beam.scanning.resolution.width,
+                self._beam.scanning.resolution.height,
+            )
+            self._txt_log.debug(
+                f"Getting standard resolution ({self._modality}): {x}, {y}."
+            )
+            return Resolution(x, y)
 
         self._txt_log.debug(
-            f"Getting {'standard' if res in self._standard_resolutions else 'extended'} resolution ({self._modality}): {str(res)}."
+            f"Getting extended resolution ({self._modality}): {self._extended_resolution}."
         )
-
-        return res
+        return self._extended_resolution
 
     @resolution.setter
     def resolution(self, value: Resolution) -> None:
+        resolution = str(value)
+        if value in self._standard_resolutions:
+            self._txt_log.debug(
+                f"Setting standard resolution to ({self._modality}): {resolution}."
+            )
+            self._beam.scanning.resolution.value = resolution
+            self._extended_resolution = None
+            return
         self._txt_log.debug(
-            f"Setting {'standard' if value in self._standard_resolutions else 'extended'} resolution ({self._modality}): {str(value)}."
+            f"Setting extended resolution to ({self._modality}): {resolution}."
         )
+        # extended resolution cannot be set directly to the beam
+        self._extended_resolution = value
 
-        self._beam.scanning.resolution.value = str(value)
+    @property
+    def extended_resolution(self) -> Resolution | None:
+        return self._extended_resolution
 
     @property
     def horizontal_field_width(self) -> float:
